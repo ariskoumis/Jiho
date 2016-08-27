e4428bf7111f11144646c9b686f3619b50123aa2
Aris <akoumis1@gmail.com>
2016-08-27 15:11:25 -0700

    user accounts system

--- jihoMeteor/client/main.js
+++ jihoMeteor/client/main.js
@@ -7,4 +7,65 @@ import './main.html';
 
 if(Meteor.isClient) {
 
+	Meteor.subscribe("userData");
+
+	Template.login.onRendered(function(){
+        $('#createAccountDiv').hide();
+        $('#forgotPasswordDiv').hide();
+        $('#createAccountForm').validate();
+        $('#loginForm').validate();
+    })
+
+    Template.login.events({
+        "submit #loginForm": function(event) {
+            event.preventDefault();
+            var email = $('[name=loginEmail]').val();
+            var password = $('[name=loginPassword]').val();
+            Meteor.loginWithPassword(email,password, function(err) {
+                if (err) {
+                    $('#loginDiv').transition('shake');
+                    alertify.alert("Error :(", err.reason);
+                }
+            });
+        },
+        "submit #createAccountForm": function(event) {
+            event.preventDefault();
+            var user = {
+                email: $('[name=email]').val(),
+                firstName: $('[name=firstName]').val(),
+                lastName: $('[name=lastName]').val(),
+                password: $('[name=password]').val(),
+                birthday: $('[name=birthday]').val()
+            }
+            Accounts.createUser(user, function(err) {
+                if (err) {
+                    $('#createAccountDiv').transition('shake');
+                    alertify.alert(err.reason)
+                } else {
+                    Meteor.loginWithPassword(user.email,user.password, function(err) {
+                        if (err) {
+                            $('#createAccountDiv').transition('shake');
+                            alertify.alert(err.reason);
+                        } else {
+                            $('.ui.basic.modal').modal('show');
+                        }
+                    });
+                }
+            });
+        },
+        "click #createAccount": function() {
+            $('#loginDiv').hide(500);
+            $('#createAccountDiv').show(500);
+        },
+        "click #forgotPassword": function() {
+            $('#loginDiv').hide(500);
+            $('#forgotPasswordDiv').show(500);
+        },
+        "click #backToLogin": function() {
+            $('#createAccountDiv').hide(500)
+            $('#forgotPasswordDiv').hide(500)
+            $('#loginDiv').show(500)
+        }
+    })
+
 }
--- jihoMeteor/server/main.js
+++ jihoMeteor/server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+  	Accounts.onCreateUser(function(options, user) {
+        user.firstName = options.firstName;
+        user.lastName = options.lastName;
+        user.birthday = options.birthday;
+        return user
+    })
+
+    Meteor.publish("userData", function () {
+        if (this.userId) {
+            return Meteor.users.find({_id: this.userId},
+            {fields: {'firstName': 1, 'lastName': 1}});
+        } else {
+            this.ready();
+        }
+    });
 });
